44e99975eb554b86d4caacd8d4cb89e60fb9b744
Ivan Kolesnikov <impalago@yandex.ru>
2015-12-10 16:59:06 +0300

    fix left menu

--- resources/views/layouts/blocks/left-panel.blade.php
+++ resources/views/layouts/blocks/left-panel.blade.php
@@ -3,7 +3,9 @@
     @include('layouts/blocks.profile-left-block')
 
     <ul class="nav nav-sidebar">
-
+        <li><a href="{{ route('ga.index') }}">Google Analytics</a></li>
+        <li><a href="{{ route('ytb.index') }}">YouTube</a></li>
+        <li><a href="{{ route('facebook.index') }}">Facebook</a></li>
     </ul>
 
 </div>
